318f73211ff945e96fd283c0eca37bcb9388366f
Chrisantha Fernando <chrisantha@chrisanthas-mbp.lan>
2014-07-05 00:55:40 +0100

    drawing pen lines now

--- test/main.lua
+++ test/main.lua
@@ -6,24 +6,24 @@ require 'hfes'
 function love.load()
 
 --Initialize the ninedot problem here. 
-	n = 2
-	k = 2
+	n = 3
+	k = 3
 	b = 5 
 	nd = hfes.ninedot(n,k,b)
 	d = hfes.hFES(nd)
 	step = 1 
 
-	 hero = {} -- new table for the hero
-	 hero.x = 300    -- x,y coordinates of the hero
-	 hero.y = 450
-	 hero.speed = 400
+	 -- hero = {} -- new table for the hero
+	 -- hero.x = 300    -- x,y coordinates of the hero
+	 -- hero.y = 450
+	 -- hero.speed = 400
 
 end
 
 function love.update(dt)
 
 	-- Start 
-	
+
 	-- Step 
 	if step <= k then 
 	 	print("doing move:" .. step)
@@ -37,11 +37,11 @@ function love.update(dt)
 	end
 
 
-	if love.keyboard.isDown("left") then
-	   hero.x = hero.x - hero.speed*dt
-	elseif love.keyboard.isDown("right") then
-	   hero.x = hero.x + hero.speed*dt
-	end
+	-- if love.keyboard.isDown("left") then
+	--    hero.x = hero.x - hero.speed*dt
+	-- elseif love.keyboard.isDown("right") then
+	--    hero.x = hero.x + hero.speed*dt
+	-- end
 
 end
 
@@ -50,6 +50,7 @@ function love.draw()
 --Get the data to draw form the problem specificiation 
 local stuffToDraw = d:getImage()
 --print(stuffToDraw)
+
 	----------------
 	--Draw the empty board + dots in the problem 
 	----------------
@@ -65,6 +66,21 @@ local stuffToDraw = d:getImage()
 			end
 		end
 	end
+
+
+	----------------
+	--Draw the lines 
+	----------------
+	love.graphics.setLineWidth( 10 )
+	if #stuffToDraw.pp > 1 then 
+		for i = 1, #stuffToDraw.pp-1 do 
+			  print("DRAWING LINE")
+			  love.graphics.line(x + 50*stuffToDraw.pp[i][1],y + 50*stuffToDraw.pp[i][2], x + 50*stuffToDraw.pp[i+1][1], y + 50*stuffToDraw.pp[i+1][2])
+
+		end	
+	end
+
+
 
  -- -- let's draw some ground
  -- love.graphics.setColor(0,255,0,255)
