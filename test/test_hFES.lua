b174dbe4afd3397e133ab4353f044a5ebbe7dd7c
Chrisantha Fernando <chrisantha@chrisanthas-mbp.lan>
2014-07-10 21:10:49 +0100

    21:10

--- test/test_hFES.lua
+++ test/test_hFES.lua
@@ -7,7 +7,11 @@ local b = 5
 local nd = hfes.ninedot(n,k,b)
 
 local d = hfes.hFES(nd)
-
+print(#nd.bs.pp)
+for j = 1, #nd.bs.pp do 
+	print(nd.bs.pp[j][1] .. " " .. nd.bs.pp[j][2])
+end
 for i = #nd.bs.pp,k-1 do 
+	print("IN MAKE MOVE")
 	d:makeMove()
 end
